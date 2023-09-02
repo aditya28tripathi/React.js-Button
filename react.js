cea42b0b7f53b72fed6f7face5423a8feea32d68
ADITYA TRIPATHI <99478857+aditya28tripathi@users.noreply.github.com>
2023-09-02 23:41:04 +0530

    Create react.js

---
+++ react.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+
+
+
+function App() {
+  return (
+  <>
+          <Button>
+            This is a button from styled-component
+          </Button>
+  
+   </>
+  );
+}
+export default App;
+
+const Button  = styled.button `
+                   background-color: black;
+                   color: white;
+                   padding: 10px;
+
+         
+
+
+
+`
